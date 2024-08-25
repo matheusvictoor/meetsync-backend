73aa867039d82458792a77c05f1a1f2adf7996ef
Iagohss <iago.silva@ccc.ufcg.edu.br>
2024-08-24 22:52:42 -0300

    feat: implementada rota de GET sala pelo id

--- src/services/roomService.ts
+++ src/services/roomService.ts
@@ -1,21 +1,29 @@
 import { Room } from '../models/room';
+import { Time } from '../models/time';
 import RoomRepository from '../repositories/roomRepository';
-import TimeService from '../services/timeService';
 
 class RoomService {
   private roomRepository;
-  private timeService: TimeService;
 
   constructor() {
     this.roomRepository = new RoomRepository();
-    this.timeService = new TimeService();
+  }
+
+  async getRoom(roomId: string) {
+    return await this.roomRepository.getRoom(roomId);
   }
 
   async createRoom(link: string, times: { date: string; start: string; end: string }[]) {
     const room = new Room(link);
-    const response = await this.roomRepository.createRoom(room);
+    const timeObjects = times.map((time) => {
+      return new Time(
+        new Date(time.date), 
+        new Date(time.start), 
+        new Date(time.end)
+      );
+    });
 
-    await this.timeService.createTimesForRoom(response.roomId, times);
+    const response = await this.roomRepository.createRoom(room, timeObjects);
     return response;
   }
 }
