c4ff287b7ee7bece3bb29339d43659973e10603d
Iagohss <iago.silva@ccc.ufcg.edu.br>
2024-09-10 17:30:42 -0300

    feat: retornar relatório de horários mais votados para Rooms com data limite excedida

--- src/services/roomService.ts
+++ src/services/roomService.ts
@@ -1,6 +1,19 @@
 import { Room } from '../models/room';
 import { Time } from '../models/time';
+import { Vote } from '../models/vote';
 import RoomRepository from '../repositories/roomRepository';
+
+type Report = {
+  times: (Time & { timeId: string })[];  
+  numVotes: number;
+}
+
+interface RoomResponse extends Room {
+  Time: (Time & {
+    timeId: string;
+    Vote: Vote[];
+  })[];
+}
 
 class RoomService {
   private roomRepository;
@@ -9,8 +22,39 @@ class RoomService {
     this.roomRepository = new RoomRepository();
   }
 
-  async getRoom(roomId: string) {
-    return await this.roomRepository.getRoom(roomId);
+  async getRoom(roomId: string): Promise<RoomResponse | Report> {
+    const room: RoomResponse | null = await this.roomRepository.getRoom(roomId);
+
+    if (!room) {
+        throw new Error('Sala indefinida');
+    }
+
+    const now = new Date();
+    if (room.endingAt < now) {
+        return this.generateReport(room);
+    }
+    return room;
+  }
+
+  generateReport(room: RoomResponse): Report {
+    let mostVoted: Report = {
+      times: [],
+      numVotes: 0
+    };
+
+    room.Time.forEach((time) => {
+      let votes = time.Vote.length;
+      if (votes > mostVoted.numVotes) {
+        mostVoted = {
+          times: [time],
+          numVotes: votes
+        };
+      } else if (votes === mostVoted.numVotes) {
+        mostVoted.times.push(time);
+      }
+    });
+
+    return mostVoted;
   }
 
   async createRoom(endingAt: string, times: { date: string; start: string; end: string }[]) {
