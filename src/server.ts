94499881513b82a5e3718dd2edd6ec233a71bba7
Matheus Victor <matheusvictor.dev@gmail.com>
2024-10-12 15:02:21 -0300

    fix: tenta corrigir o problema de cors

--- src/server.ts
+++ src/server.ts
@@ -6,23 +6,26 @@ import cors from 'cors';  // Importa o middleware cors
 
 dotenv.config();
 
+
+// Middleware para habilitar CORS
+app.use(cors({
+  origin: 'https://meet-sync-six.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true 
+}));
+
+// Middleware para habilitar CORS em todas as rotas
+app.options('*', cors({
+  origin: 'https://meet-sync-six.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
 // Rota simples para verificar se o servidor está funcionando
 app.get('/', (req, res) => {
   res.send('API MeetSync Conectada!');
-});
-
-app.use(cors({
-  origin: 'https://meet-sync-six.vercel.app', // Permitir apenas este domínio
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type'],
-  credentials: true // Se você precisa enviar cookies ou cabeçalhos de autenticação
-}));
-
-// Middleware para habilitar CORS
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'https://meet-sync-six.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.sendStatus(204); // No Content
 });
 
 // Middleware de tratamento de erros
