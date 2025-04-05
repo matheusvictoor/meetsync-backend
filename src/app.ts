1ddd29176776d5e29143356ff71b15dce0f78147
Matheus Victor <matheusvictor.dev@gmail.com>
2025-04-05 11:38:04 -0300

    fix: integrate CORS middleware with specific options and handle OPTIONS requests

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors';
 import userRoutes from "./routes/userRoutes";
 import roomRoutes from "./routes/roomRoutes";
 import voteRoutes from "./routes/voteRoutes";
@@ -6,6 +7,12 @@ import voteRoutes from "./routes/voteRoutes";
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+    origin: 'https://meetsync-pi.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
 
 app.use('/api/users', userRoutes);
 
--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,9 @@ import app from './app';
 import dotenv from "dotenv";
 import { errorMiddleware } from "./middlewares/error";
 import 'express-async-errors';
-import cors from 'cors';
 import { sendResultsToAllParticipants } from './utils/sendResultEmail';
 
 dotenv.config();
-
-// Configuração do CORS
-const corsOptions = {
-  origin: 'https://meetsync-pi.vercel.app', // Permitir apenas este domínio
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Métodos HTTP permitidos
-  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
-};
-
-app.use(cors(corsOptions));
 
 // Rota simples para verificar se o servidor está funcionando
 app.get('/', (req, res) => {
@@ -22,6 +12,11 @@ app.get('/', (req, res) => {
 });
 
 app.post('/email/:roomId', (req, res, next) => sendResultsToAllParticipants(req.params.roomId))
+
+app.options('*', (req, res, next) => {
+  console.log('Requisição OPTIONS recebida:', req.headers);
+  next();
+});
 
 // Middleware de tratamento de erros
 app.use(errorMiddleware);
